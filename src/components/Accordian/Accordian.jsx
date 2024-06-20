41353e898b3d7d358217d371eba3a4b260ca1a9f
Mohd Arshad <mohdarshad.ceo@gmail.com>
2024-06-20 16:26:28 +0530

    Accordian multi selection functionality complete

--- src/components/Accordian/Accordian.jsx
+++ src/components/Accordian/Accordian.jsx
@@ -4,20 +4,47 @@ import data from "./data";
 
 export const Accordian = () => {
   const [select, setSelect] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiSelect, setMultiSelect] = useState([]);
 
   const singleSelection = (id) => {
-    setSelect(select === id ? null : id);
+    return setSelect(select === id ? null : id);
+  };
+
+  const multiSelection = (id) => {
+    const tmp = [...multiSelect];
+    const i = tmp.indexOf(id);
+    console.log(i);
+    i === -1 ? tmp.push(id) : tmp.splice(i, 1);
+
+    return setMultiSelect(tmp);
   };
 
   return (
     <>
       <div className="wrapper">
+        <button
+          onClick={() => {
+            return (
+              setEnableMultiSelection(!enableMultiSelection),
+              enableMultiSelection ? setMultiSelect([]) : setSelect(null)
+            );
+          }}
+        >
+          {enableMultiSelection
+            ? "Disable Multi Selection"
+            : "Enable Multi Selection"}
+        </button>
         <div className="accordian">
           {data && data.length ? (
             data.map((dataitem) => {
               return (
                 <div
-                  onClick={() => singleSelection(dataitem.id)}
+                  onClick={() => {
+                    enableMultiSelection
+                      ? multiSelection(dataitem.id)
+                      : singleSelection(dataitem.id);
+                  }}
                   className="item"
                   key={dataitem.id}
                 >
@@ -26,7 +53,8 @@ export const Accordian = () => {
                     <span>+</span>
                   </div>
 
-                  {select === dataitem.id ? (
+                  {select === dataitem.id ||
+                  multiSelect.indexOf(dataitem.id) !== -1 ? (
                     <div className="content">{dataitem.description}</div>
                   ) : null}
                 </div>
